8e7607aa64e7fe4061684d011342822c4e370639
zimbapogi <educationalpurposesr@gmail.com>
2023-02-06 15:48:45 +0000

    updated the button designs

--- molePerGramGUI.py
+++ molePerGramGUI.py
@@ -19,14 +19,14 @@ class MpgCalculatorGUI:
         self.root.title("MPG Calculator")
         self.root.geometry("500x500")
         self.root.resizable(False, False)
-        self.root.config(bg="#f5f5f5")
+        self.root.config(bg="#BDD2FA")
 
         #label for the interface
-        self.header = tk.Label(self.root, text="Example: C 1 H 2", font=('monospace', 15), bg='#f5f5f5')
+        self.header = tk.Label(self.root, text="Example: C 1 H 2", font=('monospace', 15), bg='#BDD2FA', border=0)
         self.header.pack(padx=10, pady=10)
 
         #description for the textbox
-        self.textBox = tk.Text(self.root, height=1, font=('monospace', 15))
+        self.textBox = tk.Text(self.root, height=1, font=('monospace', 15), background='#BDD2FA')
         self.textBox.pack(padx=10, pady=10)
         
         #description for the image
@@ -37,7 +37,7 @@ class MpgCalculatorGUI:
         self.calcButton = tk.Button(self.root, text="Calculate", image=self.image_a)
         self.clearButton = tk.Button(self.root, text="Clear", image=self.image_b)
         self.calcButton.place(x=10, y=110, width=240, height=70)
-        self.clearButton.place(x=250, y=110, width=240, height=70)
+        self.clearButton.place(x=252, y=110, width=240, height=70)
 
         
         
